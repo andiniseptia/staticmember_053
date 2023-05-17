8c8920bd540d81fb26f9ef5b34a44dd0f10a50cd
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-17 18:40:20 +0700

    Mendefenisikan member function construktor dan destructor

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -11,3 +11,17 @@ public:
 	void cetakData();
 	void isiData();
 };
+
+//Defenisi member function
+angka::angka(int i) {  //Constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() {      //Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
