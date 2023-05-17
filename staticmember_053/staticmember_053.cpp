7fe25304b730cf4cc183834bd7620d3cd00c884b
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-17 10:55:20 +0700

    Menambahkan member class mahasiswa

---
+++ staticmember_053/staticmember_053.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void prinAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
